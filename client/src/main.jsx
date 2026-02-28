074fa215a72d96e75c1e3c1c3cf0b674de645701
gruezomen <haewodk@gmail.com>
2026-02-27 22:03:08 -0400

    configuración para despliegue correcto de estilos

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './App.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
